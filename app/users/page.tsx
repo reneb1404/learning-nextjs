cd9d58a8b7a0c2e84da3dbc54ebee635a8ab7a43
Rene Bucher <133756710+reneb1404@users.noreply.github.com>
2024-09-08 10:56:48 +0000

    Added dynamic site

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,14 @@ interface User {
 
 const UserPage = async () => {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-		next: { revalidate: 10 },
-	}); //background job, get data every 10s - only available in fetch function!!!!!!!
+		cache: "no-store", //disable caching
+	});
 	const users: User[] = await res.json();
 
 	return (
 		<>
 			<h1>Users</h1>
+			<p>{new Date().toLocaleTimeString()}</p>
 			<ul>
 				{users.map((user) => (
 					<li key={user.id}>{user.name}</li>
