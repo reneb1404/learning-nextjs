38fa4a92bdd30f328c3b25c2af07f9a38b106480
Rene Bucher <133756710+reneb1404@users.noreply.github.com>
2024-09-08 10:37:52 +0000

    Added basic client and server components

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
-	return <div>ProductCard</div>;
+	return (
+		<div>
+			<AddToCart />
+		</div>
+	);
 };
 
 export default ProductCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
 	return (
 		<main>
 			<div>Hello World!</div>
 			<Link href="/users">Users</Link>
+			<ProductCard />
 		</main>
 	);
 }
