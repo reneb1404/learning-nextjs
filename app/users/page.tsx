cd1f734a3a0e12d9684e04f63ce9f598540d397b
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-09-12 09:34:49 +0200

    Added dynamic routes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,36 +1,11 @@
 import React from "react";
-
-interface User {
-	id: number;
-	name: string;
-	email: string;
-}
+import UserTable from "./UserTable";
 
 const UserPage = async () => {
-	const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-		cache: "no-store", //disable caching
-	});
-	const users: User[] = await res.json();
-
 	return (
 		<>
 			<h1>Users</h1>
-			<table className="table table-bordered">
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>EMail</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user) => (
-						<tr key={user.id}>
-							<th>{user.name}</th>
-							<th>{user.email}</th>
-						</tr>
-					))}
-				</tbody>
-			</table>
+			<UserTable />
 		</>
 	);
 };
