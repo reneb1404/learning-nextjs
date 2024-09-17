1a9920b12fed233670c5798c47616063ff02c5d6
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-09-17 11:20:19 +0200

    added missing await to get products

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
 export async function GET(request: NextRequest) {
-	const product = prisma.product.findMany();
+	const product = await prisma.product.findMany();
 	return NextResponse.json(product);
 }
 
