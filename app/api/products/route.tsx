ae8429384d7c960a39473c006dedd9bad9b46c33
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-09-17 11:15:56 +0200

    added product storing in db, update and delete still missing

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,11 @@
+import ProductCard from "@/app/components/ProductCard";
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
-export function GET(request: NextRequest) {
-	return NextResponse.json([
-		{ id: 1, name: "Milk", price: 2.5 },
-		{ id: 2, Name: "Bread", price: 3.5 },
-	]);
+export async function GET(request: NextRequest) {
+	const product = prisma.product.findMany();
+	return NextResponse.json(product);
 }
 
 export async function POST(request: NextRequest) {
@@ -13,8 +13,13 @@ export async function POST(request: NextRequest) {
 	const valid = schema.safeParse(body);
 	if (!valid.success)
 		return NextResponse.json(valid.error.errors, { status: 400 });
-	return NextResponse.json(
-		{ id: 10, name: body.name, price: body.price },
-		{ status: 201 }
-	);
+
+	const newProduct = await prisma.product.create({
+		data: {
+			name: body.name,
+			price: body.price,
+		},
+	});
+
+	return NextResponse.json(newProduct);
 }
