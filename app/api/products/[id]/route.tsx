a37fef1e9304a3ddafd44ca19fa0e03824937b6d
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-09-17 11:46:14 +0200

    Added update and delete api endpoint for products

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,2 +1,67 @@
-//Do at home
-//Can't  test functionality
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+
+interface Props {
+	params: { id: string };
+}
+
+export async function GET(
+	request: NextResponse,
+	{ params }: { params: { id: string } }
+) {
+	const product = await prisma.product.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+	if (!product)
+		return NextResponse.json(
+			{ error: "Product doesn't exist" },
+			{ status: 404 }
+		);
+
+	return NextResponse.json(product);
+}
+
+export async function PUT(
+	request: NextResponse,
+	{ params }: { params: { id: string } }
+) {
+	const body = await request.json();
+	const valid = schema.safeParse(body);
+	if (!valid.success)
+		return NextResponse.json(valid.error.errors, { status: 400 });
+
+	const product = await prisma.product.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	if (!product)
+		return NextResponse.json({ error: "Product not found" }, { status: 404 });
+
+	const updateProduct = await prisma.product.update({
+		where: { id: product.id },
+		data: {
+			name: body.name,
+			price: body.price,
+		},
+	});
+
+	return NextResponse.json(updateProduct);
+}
+export async function DELETE(
+	request: NextResponse,
+	{ params }: { params: { id: string } }
+) {
+	const product = await prisma.product.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	if (!product)
+		return NextResponse.json({ erorr: "Product not found" }, { status: 404 });
+
+	const deleteProduct = await prisma.product.delete({
+		where: { id: product.id },
+	});
+
+	return NextResponse.json(deleteProduct);
+}
