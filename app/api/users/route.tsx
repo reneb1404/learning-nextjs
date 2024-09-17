c854ccea9862903b88d5b85be13993236a3dd019
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2024-09-17 10:31:48 +0200

    Changed get users api to database instead of hard-coded

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,10 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
-export function GET(request: NextRequest) {
-	return NextResponse.json([
-		{ id: 1, name: "René" },
-		{ id: 2, name: "Pete" },
-	]);
+export async function GET(request: NextRequest) {
+	const users = await prisma.user.findMany();
+	return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
